84af76f42fb07ad5fe3b9d9ca744b7cc004b0a8e
uesho1199 <145344392+uesho1199@users.noreply.github.com>
2023-09-18 20:23:37 +0900

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,24 +1,41 @@
-package com.example.testapp5
+package com.example.testapp4
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //inputText
+        var editText = findViewById<EditText>(R.id.edit_text)
+        var editText2 = findViewById<EditText>(R.id.edit_text2)
+        var inputName = editText.editableText
+        var inputPass = editText2.editableText
+
         //ボタンコード
         val button: Button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
 
-            //クリック時の処理
-            val intent = Intent(this,MainActivity2::class.java)
-            startActivity(intent)
+            if(editText.text != null) {
+                //クリック時の処理
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("INPUT_NAME", inputName.toString())
+                intent.putExtra("INPUT_PASS", inputPass.toString())
 
+                if(inputName.toString().length != 0 && inputPass.toString().length != 0){
+                    startActivity(intent)
+                }
+            }
         }
+
+        //チェックボックス
+        val checkBox: CheckBox = findViewById(R.id.checkbox);
 
     }
 }
--- MainActivity2.kt
+++ MainActivity2.kt
@@ -1,13 +1,22 @@
-package com.example.testapp5
+package com.example.testapp4
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        var getName = findViewById<TextView>(R.id.get_text)
+        var getPass = findViewById<TextView>(R.id.get_text2)
+        val intentName = intent.getStringExtra("INPUT_NAME")
+        val intentPass = intent.getStringExtra("INPUT_PASS")
+
+        getName.text = intentName
+        getPass.text = intentPass
 
         val button2: Button = findViewById<Button>(R.id.button2)
         button2.setOnClickListener {
