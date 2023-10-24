e80b0f0bbf6b3f1be58c9b1dc19364f186bc98b4
uesho1199 <145344392+uesho1199@users.noreply.github.com>
2023-10-24 16:20:43 +0900

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -27,25 +27,25 @@ class MainActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.button)
         checkBox = findViewById(R.id.checkbox)
 
-        // 以前に保存された名前とパスワードを取得
+        // 以前保存された名前とパスワードを取得
         val sharedPreferences = getSharedPreferences("user_info", MODE_PRIVATE)
         savedName = sharedPreferences.getString("NAME", "").toString()
         savedPass = sharedPreferences.getString("PASS", "").toString()
 
-        // もし保存された名前とパスワードがない場合、新しい情報を入力し保存する
+        // 保存された名前とパスワードがない場合、新しい情報を入力し保存する
         if (savedName.isEmpty() || savedPass.isEmpty()) {
             button.setOnClickListener {
                 val inputName = editText.text.toString()
                 val inputPass = editText2.text.toString()
 
                 if (inputName.isNotEmpty() && inputPass.isNotEmpty() && inputPass.length >= 8 && inputPass.length <= 12) {
-                    // パスワードが条件を満たす場合、情報を保存
+                    // パスワードの条件を満たす場合、情報を保存
                     val editor = sharedPreferences.edit()
                     editor.putString("NAME", inputName)
                     editor.putString("PASS", inputPass)
                     editor.apply()
 
-                    // 次画面に遷移
+                    // Main2画面に遷移
                     val intent = Intent(this, MainActivity2::class.java)
                     intent.putExtra("INPUT_NAME", inputName)
                     intent.putExtra("INPUT_PASS", inputPass)
@@ -85,7 +85,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        // チェックボックスがチェックされている場合、SharedPreferencesから名前を読み取りEditTextにセットする
+        // チェックボックスにチェックがある場合、SharedPreferencesから名前を読み取りEditTextにセットする
         val sharedPreferences = getSharedPreferences("user_info", MODE_PRIVATE)
 
         // パスワードフィールドをクリアする
